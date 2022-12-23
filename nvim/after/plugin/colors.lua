5bc6202a06286a1344205f513b9b7aca4cc33d3e
Rodrigo Munoz <rodrigo.munoz@adhocteam.us>
2022-12-22 18:47:50 -0800

    update colors for CursorLine. telescope merge conflicts

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,9 +1,10 @@
 require("catppuccin").setup({
-  transparent_background = true,
-  term_colors = true,
+	transparent_background = true,
+	term_colors = true,
 })
 
 -- setup must be called before loading
-vim.cmd.colorscheme "catppuccin"
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.cmd.colorscheme("catppuccin")
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "#2e2e3f" })
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
