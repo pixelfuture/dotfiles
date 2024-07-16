4f1a9cacb04cc09d88725b2fa0d13594996714d9
Rodrigo Munoz <rodrigo.munoz@adhocteam.us>
2024-07-16 09:33:20 -0700

    add fast wrap

--- nvim/lua/hotrod/plugins/nvim-autopairs.lua
+++ nvim/lua/hotrod/plugins/nvim-autopairs.lua
@@ -1,5 +1,16 @@
 return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
-	opts = {},
+	opts = {
+		fast_wrap = {
+			map = "<M-e>",
+			chars = { "{", "[", "(", '"', "'", "`" },
+			pattern = [=[[%'%"%>%]%)%}%,]]=],
+			end_key = "$",
+			keys = "qwertyuiopzxcvbnmasdfghjkl",
+			check_comma = true,
+			highlight = "Search",
+			highlight_grey = "Comment",
+		},
+	},
 }
